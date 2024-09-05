2617efb382a3706475c8fd460da395bdf771e9d4
Shashwath Udaya Kumar <46096060+shashwathuday@users.noreply.github.com>
2024-09-05 13:27:49 -0400

    Update 3 Sum.py

--- Solutions/3 Sum/3 Sum.py
+++ Solutions/3 Sum/3 Sum.py
@@ -1,5 +1,5 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def threeSum(self, nums: List[int]) -> List[List[int]]: # type: ignore
         res = []
         nums.sort()
 
